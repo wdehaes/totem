200df0cf7f5ce80a0aaa297f0e910b1e03aac2d8
Willem Dehaes <willem@datacamp.com>
2017-02-03 20:20:06 +0100

    refactor using OO CSS

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
+
   $('.main-menu').hover(
     function() {
       $('.main').addClass("side-margin");
+      $('.nav-text').removeClass("hidden");
     },
     function() {
       $('.main').removeClass("side-margin");
+      $('.nav-text').addClass("hidden");
     }
 
     );
